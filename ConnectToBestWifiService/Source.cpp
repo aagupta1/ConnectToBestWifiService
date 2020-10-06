aacbbd5141190795788b76aaa27c6aff05211f3e
gupta <gupta@DESKTOP-2SIPPCU>
2020-10-06 10:05:49 +0530

    Adding logging to file

--- ConnectToBestWifiService/Source.cpp
+++ ConnectToBestWifiService/Source.cpp
@@ -1,19 +1,28 @@
+#pragma once
+
 #ifndef UNICODE
 #define UNICODE
 #endif
 
-#include <windows.h>
+#include <Windows.h>
 #include <wlanapi.h>
 #include <objbase.h>
 #include <wtypes.h>
-#include <Windows.h>
 #include <tchar.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <TraceLoggingProvider.h>  
+#include <strsafe.h>
+#include <wmistr.h>
+#include <evntrace.h>
+#include <evntprov.h>
 
-// Need to link with Wlanapi.lib and Ole32.lib
+
+// Need to link with Wlanapi.lib and Ole32.lib and advapi.lib
 #pragma comment(lib, "wlanapi.lib")
 #pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "advapi32.lib")
 
 SERVICE_STATUS        g_ServiceStatus = { 0 };
 SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
@@ -25,12 +34,50 @@ DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
 
 #define SERVICE_NAME  _T("ConnectToBestWifiService \n")
 
+//Handle for log file
+HANDLE hFile;
 
+void logMessage(char message[512])
+{
 
+    DWORD dwBytesToWrite = (DWORD)strlen(message);
+    DWORD dwBytesWritten = 0;
+    BOOL bErrorFlag = FALSE;
+
+    if (INVALID_HANDLE_VALUE == hFile) {
+        OutputDebugString(_T("Unable to write to log file - file handle is invalid \n"));
+        return;
+    }
+
+    bErrorFlag = WriteFile(
+        hFile,
+        message,
+        dwBytesToWrite,
+        &dwBytesWritten,
+        NULL);
+
+    if (FALSE == bErrorFlag)
+    {
+        OutputDebugString(_T("Unable to write to log file \n"));
+    }
+    else
+    {
+        if (dwBytesWritten != dwBytesToWrite)
+        {
+            // This is an error because a synchronous write that results in
+            // success (WriteFile returns TRUE) should write all data as
+            // requested. This would not necessarily be the case for
+            // asynchronous writes.
+            OutputDebugString(_T("Error: dwBytesWritten != dwBytesToWrite\n"));
+        }
+    }
+}
 
 int _tmain(int argc, TCHAR* argv[])
 {
     OutputDebugString(_T("ConnectToBestWifiService: Main: Entry \n"));
+
+    //setupTraceLogging();
 
     SERVICE_TABLE_ENTRY ServiceTable[] =
     {
@@ -173,6 +220,9 @@ VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
             OutputDebugString(_T("ConnectToBestWifiService: ServiceCtrlHandler: SetServiceStatus returned error \n"));
         }
 
+        // Close Log file handle
+        CloseHandle(hFile);
+
         // This will signal the worker thread to start shutting down
         SetEvent(g_ServiceStopEvent);
 
@@ -212,6 +262,7 @@ int ConnectToBestWifi(HANDLE hClient) {
         char msgbuf[512];
         sprintf_s(msgbuf, "WlanEnumInterfaces failed with error: %u\n", dwResult);
         OutputDebugStringA((LPCSTR)msgbuf);
+        logMessage(msgbuf);
         return 1;
     }
     else {
@@ -219,6 +270,7 @@ int ConnectToBestWifi(HANDLE hClient) {
         char msgbuf[512];
         sprintf_s(msgbuf, "\n\n Num Of Interfaces: %lu\n", pIfList->dwNumberOfItems);
         OutputDebugStringA((LPCSTR)msgbuf);
+        logMessage(msgbuf);
 
         unsigned int sleepIntervalInSeconds = 0;
 
@@ -236,17 +288,20 @@ int ConnectToBestWifi(HANDLE hClient) {
                 char msgbuf[512];
                 sprintf_s(msgbuf, "StringFromGUID2 failed\n");
                 OutputDebugStringA((LPCSTR)msgbuf);
+                logMessage(msgbuf);
             }
             else {
                 char msgbuf[512];
                 sprintf_s(msgbuf, "InterfaceGUID[%d]: %ws\n", i, GuidString);
                 OutputDebugStringA((LPCSTR)msgbuf);
+                logMessage(msgbuf);
             }
 
             char msgbuf[512];
-            sprintf_s(msgbuf, "Interface Description[%d]: %ws", i,
+            sprintf_s(msgbuf, "Interface Description[%d]: %ws \n", i,
                 pIfInfo->strInterfaceDescription);
             OutputDebugStringA((LPCSTR)msgbuf);
+            logMessage(msgbuf);
 
             OutputDebugString(_T("\n"));
 
@@ -262,9 +317,11 @@ int ConnectToBestWifi(HANDLE hClient) {
                 sprintf_s(msgbuf, "WlanScan failed with error : % u\n",
                     dwResult);
                 OutputDebugStringA((LPCSTR)msgbuf);
+                logMessage(msgbuf);
                 return 1;
             }
 
+            
             dwResult = WlanGetAvailableNetworkList(hClient,
                 &pIfInfo->InterfaceGuid,
                 WLAN_AVAILABLE_NETWORK_INCLUDE_ALL_ADHOC_PROFILES || WLAN_AVAILABLE_NETWORK_INCLUDE_ALL_MANUAL_HIDDEN_PROFILES,
@@ -277,6 +334,7 @@ int ConnectToBestWifi(HANDLE hClient) {
                 sprintf_s(msgbuf, "WlanGetAvailableNetworkList failed with error : % u\n",
                     dwResult);
                 OutputDebugStringA((LPCSTR)msgbuf);
+                logMessage(msgbuf);
 
                 dwRetVal = 1;
             }
@@ -286,6 +344,7 @@ int ConnectToBestWifi(HANDLE hClient) {
                 char msgbuf[512];
                 sprintf_s(msgbuf, "\nNum Of Available Wifi Networks: %lu\n", pBssList->dwNumberOfItems);
                 OutputDebugStringA((LPCSTR)msgbuf);
+                logMessage(msgbuf);
 
                 for (j = 0; j < pBssList->dwNumberOfItems; j++) {
                     pBssEntry =
@@ -294,9 +353,11 @@ int ConnectToBestWifi(HANDLE hClient) {
                     char msgbuf[512];
                     sprintf_s(msgbuf, "Profile Name[%u]: %ws\n", j, pBssEntry->strProfileName);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
 
                     sprintf_s(msgbuf, "SSID[%u]: \t", j);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
 
                     if (pBssEntry->dot11Ssid.uSSIDLength == 0)
                         OutputDebugString(_T("\n"));
@@ -305,13 +366,16 @@ int ConnectToBestWifi(HANDLE hClient) {
                             char msgbuf[512];
                             sprintf_s(msgbuf, "%c", (int)pBssEntry->dot11Ssid.ucSSID[k]);
                             OutputDebugStringA((LPCSTR)msgbuf);
+                            logMessage(msgbuf);
                         }
                         OutputDebugString(_T("\n"));
+                        logMessage((char *)"\n");
                     }
 
                     sprintf_s(msgbuf, "  Signal Quality[%u]:\t %u \n", j,
                         pBssEntry->wlanSignalQuality);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
 
                     if (pBssEntry->wlanSignalQuality > bestSignalQuality && pBssEntry->strProfileName[0] != '\0' && pBssEntry->bNetworkConnectable) {
                         bestSignalQuality = pBssEntry->wlanSignalQuality;
@@ -321,19 +385,23 @@ int ConnectToBestWifi(HANDLE hClient) {
                     if (pBssEntry->dwFlags) {
                         if (pBssEntry->dwFlags & WLAN_AVAILABLE_NETWORK_CONNECTED) {
                             OutputDebugString(_T(" - Currently connected"));
+                            logMessage((char *)" - Currently connected");
                             currentlyConnectedProfile = pBssEntry->strProfileName;
                         }
 
                         if (pBssEntry->dwFlags & WLAN_AVAILABLE_NETWORK_HAS_PROFILE)
                             OutputDebugString(_T(" - Has profile"));
+                            logMessage((char*)" - Has profile");
                     }
 
                     OutputDebugString(_T("\n\n"));
+                    logMessage((char*)"\n\n");
                 }
             }
 
             sprintf_s(msgbuf, "Best signal quality %u, best profile: %ws\n", bestSignalQuality, bestProfile);
             OutputDebugStringA((LPCSTR)msgbuf);
+            logMessage(msgbuf);
 
             if (bestProfile == NULL) {
                 continue;
@@ -341,6 +409,7 @@ int ConnectToBestWifi(HANDLE hClient) {
 
             if (currentlyConnectedProfile != NULL && *currentlyConnectedProfile == *bestProfile) {
                 OutputDebugString(_T("Already connected to the best available network \n"));
+                logMessage((char*)"Already connected to the best available network \n");
             }
 
             if (currentlyConnectedProfile == NULL || *currentlyConnectedProfile != *bestProfile) {
@@ -350,12 +419,14 @@ int ConnectToBestWifi(HANDLE hClient) {
                 dwResult = WlanDisconnect(hClient, &pIfInfo->InterfaceGuid, NULL);
                 if (dwResult != ERROR_SUCCESS) {
                     char msgbuf[512];
-                    sprintf_s(msgbuf, "WlanEnumInterfaces failed with error: %u\n", dwResult);
+                    sprintf_s(msgbuf, "WlanDisconnect failed with error: %u\n", dwResult);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
                     return 1;
                 }
                 else {
                     OutputDebugString(_T("Successfully disconnected currently connected network \n"));
+                    logMessage((char*)"Successfully disconnected currently connected network \n");
                 }
 
                 //Connect to network with best signal quality
@@ -373,12 +444,14 @@ int ConnectToBestWifi(HANDLE hClient) {
                     char msgbuf[512];
                     sprintf_s(msgbuf, "Wlan connect failed with error: %u\n", dwResult);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
                     return 1;
                 }
                 else {
                     char msgbuf[512];
                     sprintf_s(msgbuf, "Successfully connected to %ws \n\n\n", cp.strProfile);
                     OutputDebugStringA((LPCSTR)msgbuf);
+                    logMessage(msgbuf);
                 }
             }
 
@@ -400,17 +473,33 @@ int ConnectToBestWifi(HANDLE hClient) {
             WlanFreeMemory(pIfList);
             pIfList = NULL;
         }
-
         return 0;
     }
 }
 
 
+
 DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
 {
-    // Sleep(60000); // uncomment this to debug the app
+    Sleep(60000); // uncomment this to debug the app
     OutputDebugString(_T("ConnectToBestWifiService: ServiceWorkerThread: Entry \n"));
 
+    hFile = CreateFile(L"ConnectToBestWifiService.log",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        OutputDebugString(_T("ConnectToBestWifiService: ServiceWorkerThread: Unable to open log file for writing \n"));
+    }
+    
+    WCHAR Path[512];
+    GetFinalPathNameByHandle(hFile, Path, 512, VOLUME_NAME_NT);
+           
     DWORD dwMaxClient = 2;
     DWORD dwCurVersion = 0;
     HANDLE hClient = NULL;
